6f321ec0f9b62e8ce3ff7a9c1e800333daddd6fd
Omer Khan <notomerkhan@gmail.com>
2024-07-03 03:40:55 -0400

    implemented keyboard functions

--- Macgic/Macgic/AppDelegate.swift
+++ Macgic/Macgic/AppDelegate.swift
@@ -15,6 +15,55 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.setFrameAutosaveName("Main Window")
         window.contentView = NSHostingView(rootView: contentView)
         window.makeKeyAndOrderFront(nil)
+
+        setupMainMenu()
+    }
+
+    func setupMainMenu() {
+        let mainMenu = NSMenu()
+
+        // App menu
+        let appMenuItem = NSMenuItem()
+        mainMenu.addItem(appMenuItem)
+
+        let appMenu = NSMenu(title: "Application")
+        appMenuItem.submenu = appMenu
+        appMenu.addItem(withTitle: "About MyApp", action: #selector(NSApplication.orderFrontStandardAboutPanel(_:)), keyEquivalent: "")
+        appMenu.addItem(NSMenuItem.separator())
+        appMenu.addItem(withTitle: "Quit MyApp", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+
+        // File menu
+        let fileMenuItem = NSMenuItem()
+        mainMenu.addItem(fileMenuItem)
+
+        let fileMenu = NSMenu(title: "File")
+        fileMenuItem.submenu = fileMenu
+        fileMenu.addItem(withTitle: "Close", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w")
+
+        // Edit menu
+        let editMenuItem = NSMenuItem()
+        mainMenu.addItem(editMenuItem)
+
+        let editMenu = NSMenu(title: "Edit")
+        editMenuItem.submenu = editMenu
+        editMenu.addItem(withTitle: "Undo", action: #selector(undo), keyEquivalent: "z")
+        editMenu.addItem(withTitle: "Redo", action: #selector(redo), keyEquivalent: "Z")
+        editMenu.addItem(NSMenuItem.separator())
+        editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+        editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+
+        // Add the main menu to the application
+        NSApp.mainMenu = mainMenu
+    }
+
+    @objc func undo() {
+        // Implement undo functionality
+    }
+
+    @objc func redo() {
+        // Implement redo functionality
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
